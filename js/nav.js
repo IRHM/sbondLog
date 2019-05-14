1b4bc88fde1b1bbf754d9f2448cfe58f48f125bb
sbondo1234 <37304121+sbondo1234@users.noreply.github.com>
2019-05-14 02:43:50 +0100

    new nav
    - responsive (hamburger)
    - search bar (nothing to search yet)
    - nice!

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+function searchToggle(e) {
+  document.getElementById("searchTog").classList.toggle('hidden');
+}
+
+function hamburgerToggle(e) {
+  document.getElementById("hamburger-drop").classList.toggle('hidden');
+}
+
+function hamburgerToggleSearch(e) {
+  document.getElementById("hamburger-drop-search").classList.toggle('hidden');
+}
