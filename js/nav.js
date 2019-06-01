a0f2d3a748a3ddddb796ac23d633bac843a5fcb7
sbondo1234 <37304121+sbondo1234@users.noreply.github.com>
2019-06-01 21:12:55 +0100

    scrapping idea
    - not going to do this anymore
    BUT DID:
    - New Scrollbar
    - Sort Categories Alphabetically
    - Update Hugo Version

--- js/nav.js
+++ js/nav.js
@@ -8,7 +8,3 @@ function hamburgerToggle(e) {
   document.getElementById("hamburger-drop").classList.toggle('hidden');
   document.getElementById("searchTog").classList.toggle('hidden');
 }
-
-function projToggle(e) {
-  document.getElementById("projTog").classList.toggle('hidden');
-}
