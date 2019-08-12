e5117a4cf08f33c67935a0c810cb99c1fce02f4c
Mr <37304121+sbondo1234@users.noreply.github.com>
2019-08-12 03:59:42 +0100

    match+

---
+++ 403.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
+<link href="https://log.sbond.ml/css/tachyons.min.css" rel="stylesheet" type="text/css">
+</head>
+<body class="sans-serif mv5-ns">
+	<center>
+		<img src="https://ul.sbond.ml/i/gif/homer-punch.gif" 
+			style="height:250px;width:300px;"></img>
+		
+		<h1 style="color: #292929;">403. That’s an error.</h1>
+		
+		<h5 style="color: #292929;">You don't have permission to 
+		access <strong><?php echo $_SERVER['REQUEST_URI']; ?></strong>
+		on this server.</h5>
+	</center>
+</body>
+</html>	
