dd239ea2cfebd843562bdc2bd664575054494fae
sbondo1234 <37304121+sbondo1234@users.noreply.github.com>
2020-04-18 05:41:44 +0100

    fix searchbar being hidden on hamburger click bug

--- static/js/nav.js
+++ static/js/nav.js
@@ -3,12 +3,25 @@ let searchbar = document.getElementById("search");
 let chickenburger = document.getElementById("chickenburger");
 let chickenburgerMenu = document.getElementById("chickenburgerMenu");
 
-search.addEventListener("click", function() {
-    searchbar.classList.toggle('hidden');
+search.addEventListener("click", function () {
+    if (searchbar.classList.contains("hidden")) {
+        searchbar.classList.remove("hidden");
+    }
+    else {
+        searchbar.classList.add("hidden");
+    }
+
+    chickenburgerMenu.classList.add('hidden');
     searchbar.focus();
 });
 
-chickenburger.addEventListener("click", function() {
-    chickenburgerMenu.classList.toggle('hidden');
-    searchbar.classList.toggle('hidden');
+chickenburger.addEventListener("click", function () {
+    if (chickenburgerMenu.classList.contains("hidden")) {
+        searchbar.classList.remove("hidden");
+        chickenburgerMenu.classList.remove('hidden');
+    }
+    else {
+        chickenburgerMenu.classList.add('hidden');
+        searchbar.classList.add("hidden");
+    }
 });
