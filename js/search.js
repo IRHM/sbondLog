407cd36ff51ca4e675d58a728eac3a7aeea555b3
sbondo1234 <37304121+sbondo1234@users.noreply.github.com>
2020-04-20 00:15:08 +0100

    search.js through index.json
    extension used to minify js, doesn't work when using async methods, so going to have to use non-minified bundle for now.

--- js/search.js
+++ js/search.js
@@ -1,1 +1,23 @@
-﻿
+﻿async function getIndex() {
+    let response = await fetch('https://log.sbond.co/index.json', { method: "GET" });
+
+    return await response.json();
+}
+
+async function searchFor(query) {
+    let json = await getIndex();
+
+    //var results = [];results.push(json[i]);
+
+    for (var i = 0; i < json.length; i++) {
+        if (JSON.stringify(json[i]).includes(query)) {
+            console.log("%c match: " + json[i]["title"], 'color: green');
+            console.log(json[i]["summary"]);
+        }
+        else {
+            console.log("%c not match: " + json[i]["title"], 'color: red');
+        }
+    }
+}
+
+searchFor("talk");
